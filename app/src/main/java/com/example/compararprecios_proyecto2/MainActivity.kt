1ae2385ad48dc17fc2741fb3cdd6f0ddd814d0d0
jack.daga <3Nc0ntr4d05_9>
2020-06-05 00:44:19 -0500

    App Official - Compara Precios v1.0

--- app/src/main/java/com/example/compararprecios_proyecto2/MainActivity.kt
+++ app/src/main/java/com/example/compararprecios_proyecto2/MainActivity.kt
@@ -6,6 +6,12 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //Splash
+        //solo usar para el ej, ya que cuando la app esté más cargada tendrá tiempo para que se cargue.
+        Thread.sleep(2000)
+        setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
